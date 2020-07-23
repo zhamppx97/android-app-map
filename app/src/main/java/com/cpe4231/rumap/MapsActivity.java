594f328697892680557c3ce16128c3782b427ee4
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-07-23 13:54:04 +0700

    init commit

---
+++ app/src/main/java/com/cpe4231/rumap/MapsActivity.java
@@ -0,0 +1,50 @@
+package com.cpe4231.rumap;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng LTB = new LatLng(13.754458, 100.620443);
+        mMap.addMarker(new MarkerOptions().position(LTB).title("Marker in LTB Building Huamak"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(LTB));
+        mMap.setMinZoomPreference(6.0f);
+        mMap.setMaxZoomPreference(20.0f);
+        mMap.setTrafficEnabled(true);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LTB , 18));
+    }
+}
